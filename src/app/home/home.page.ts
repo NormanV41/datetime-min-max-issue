a0cc3421d20fcfccc88b46c45f5d5571c3254d11
Norman Vasquez <nrvasquez41@hotmail.com>
2021-10-30 17:16:44 -0600

    showing min/max issue

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage  {
 
-  constructor() {}
+
+  constructor() {
+  }
 
 }
